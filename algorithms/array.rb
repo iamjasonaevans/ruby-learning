122dccf44c38bb33d453bc943b8b03457075dbb2
Jason A. Evans <mr.jman@gmail.com>
2016-02-16 17:14:46 +0800

    Added Merge Sort Algorithm

--- algorithms/array.rb
+++ algorithms/array.rb
@@ -6,10 +6,14 @@ class Array
   #
   # T(n) = O(log n)
   #
-  # note: Ruby 2.1 has a built in binary search array#bsearch
+  # Note: Ruby 2.1 has a built in binary search array#bsearch
   #
-  # value, the value to search for
-  # returns: int representing the index if the value
+  # params:
+  #   value: (required) the value to search for
+  #   min: the lhs index of the array
+  #   max: the rhs index of the array
+  # returns:
+  #   int representing the index if the value
   #
   def binary_search(value, min = 0, max = (length - 1))
     return nil if (max < min)
@@ -24,4 +28,32 @@ class Array
       mid
     end
   end
+
+  # Quick Sort
+  #
+  # T(n) = O(n log n)
+  def merge_sort
+    return self if self.size <= 1
+
+    mid = (self.length / 2).floor
+
+    # divide the array
+    lhs = self.slice(0, mid).merge_sort
+    rhs = self.slice(mid, self.length).merge_sort
+
+    merge(lhs, rhs)
+  end
+
+  def merge(left, right)
+    result = []
+    until left.empty? || right.empty?
+      if left.first <= right.first
+        result << left.shift
+      else
+        result << right.shift
+      end
+    end
+    result + left + right
+  end
 end
+
--- test/algorithms/array_test.rb
+++ test/algorithms/array_test.rb
@@ -7,4 +7,17 @@ class AlgorithmsArrayTest < MiniTest::Test
     result = a.binary_search(100)
     assert_equal result, 7
   end
+
+  def test_merge_sort
+    a = [4,9,22,101,45,58,87,68]
+    result = a.merge_sort()
+    assert_equal result, [4,9,22,45,58,68,87,101]
+  end
+
+  def test_merge_sort2
+    a = [4, 99, 5, 1000, 2993939]
+    result = a.merge_sort()
+    assert_equal result, [4,5,99,1000,2993939]
+  end
+
 end
